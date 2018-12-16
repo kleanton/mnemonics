8dde5c6b843ff2f184ec10c416c08090d4464794
Anton Kleyman <kleanton@gmail.com>
2018-12-16 08:20:53 +0300

    program architecture

---
+++ play.py
@@ -0,0 +1,34 @@
+import pygame
+import random
+
+def gameStart():
+    drawWindow()
+    board=[]
+    drawBoard(board)
+    GetScore()
+    whoeGoesFirst()
+    pass
+
+def drawWindow():
+    pass
+
+def drawBoard(board):
+    pass
+
+def getNewBoard():
+    pass
+
+def isOnboard(x,y):
+    pass
+
+def GetScore():
+    pass
+
+def whoeGoesFirst():
+    pass
+
+def GetComputersMove():
+    pass
+
+
+gameStart()
